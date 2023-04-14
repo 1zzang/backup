b3df1fa6a3ff42cf38722c0c08ac63250b26ed25
1zzang <we1comerain@kakao.com>
2023-04-15 00:58:10 +0900

    day2-4

---
+++ javascript/day3.js
@@ -0,0 +1,17 @@
+function func0(){
+    basic.style.display="block";
+    man.style.display="none";
+    woman.style.display="none";
+}
+
+function func1(){
+    basic.style.display="none";
+    man.style.display="block";
+    woman.style.display="none";
+}
+
+function func2(){
+    basic.style.display="none";
+    man.style.display="none";
+    woman.style.display="block";
+}
